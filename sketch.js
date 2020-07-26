1b6e4cc580662bd39ab817ff348ebcd76783b246
keerthanavelan <67827550+keerthanavelan@users.noreply.github.com>
2020-07-26 16:27:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,84 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var boxPart1,boxPart2,boxPart3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	boxPart1 = createSprite(300,600,20,100);
+	boxPart2 = createSprite(500,600,20,100);
+	boxPart3 = createSprite(400,650,200,20);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	World.add(world, ground);
+
+	
+
+	Engine.run(engine);
+	
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x
+  packageSprite.y= packageBody.position.y
+
+	// if (isTouching(packageBody,boxPart3)){
+	// 	Matter.Body.setStatic(packageBody,true);
+	// }
+	// isTouching();
+
+	keyPressed();
+	drawSprites();
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(packageBody,false);
+		packageBody.scale = 0.5;
+	}
+}
+
+// function isTouching(object1,object2) {
+//     if  (object1.position.x-object2.position.x < object1.width/2 + object2.width/2 
+//       && object2.position.x-object1.position.x < object1.width/2 + object2.width/2
+//       && object1.position.y-object2.position.y < object1.height/2 + object2.height/2 
+//       && object2.position.y-object1.position.y < object1.height/2 + object2.height/2){
+//         return true;
+//     }
+//     else {
+//       return false;
+//     }
+//   }
